0171e2b02c03a474e862acd827be487ff1b7e604
Leandro Real <leo.real2585@gmail.com>
2020-12-18 10:52:07 -0300

    test: SendForgotPasswordEmail initialized

--- server/src/modules/users/services/AuthenticateUserService.ts
+++ server/src/modules/users/services/AuthenticateUserService.ts
@@ -1,7 +1,7 @@
 import { sign } from 'jsonwebtoken';
+import { injectable, inject } from 'tsyringe';
 import authConfig from '@config/auth';
 import AppError from '@shared/errors/AppError';
-import { injectable, inject } from 'tsyringe';
 import User from '../infra/typeorm/entities/User';
 import IUsersRepository from '../repositories/IUsersRepository';
 import IHashProvider from '../providers/HashProvider/models/IHashProvider';
