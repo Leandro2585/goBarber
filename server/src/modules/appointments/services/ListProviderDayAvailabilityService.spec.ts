908a4678462693fd6bd2fbae65ad00a291c68b2a
Leandro Real <leo.real2585@gmail.com>
2021-01-04 10:36:26 -0300

    test: add ListProviderDayAvailability

---
+++ server/src/modules/appointments/services/ListProviderDayAvailabilityService.spec.ts
@@ -0,0 +1,42 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import ListProviderDayAvailabilityService from './ListProviderDayAvailabilityService';
+import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
+
+let fakeAppointmentsRepository: FakeAppointmentsRepository;
+let listProviderDayAvailability: ListProviderDayAvailabilityService;
+
+describe('ListProviderDayAvailability', () => {
+  beforeEach(() => {
+    fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    listProviderDayAvailability = new ListProviderDayAvailabilityService(
+      fakeAppointmentsRepository,
+    );
+  });
+  it('should be able to list the month availability from provider', async () => {
+    await fakeAppointmentsRepository.create({
+      provider_id: 'user',
+      date: new Date(2021, 0, 1, 8, 0, 0),
+    });
+
+    await fakeAppointmentsRepository.create({
+      provider_id: 'user',
+      date: new Date(2021, 0, 1, 10, 0, 0),
+    });
+
+    const availability = await listProviderDayAvailability.execute({
+      provider_id: 'user',
+      day: 1,
+      month: 1,
+      year: 2020,
+    });
+
+    expect(availability).toEqual(
+      expect.arrayContaining([
+        { hour: 8, available: false },
+        { hour: 9, available: true },
+        { hour: 10, available: false },
+        { hour: 11, available: true },
+      ]),
+    );
+  });
+});
