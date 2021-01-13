0bb0f917ef32d49266928d0f0171d791f7ce83e8
Leandro Real <leo.real2585@gmail.com>
2021-01-12 22:32:51 -0300

    chore: add SESMailProvider

---
+++ server/src/config/mail.ts
@@ -0,0 +1,20 @@
+interface IMailConfig {
+  driver: 'ethereal' | 'ses';
+  defaults: {
+    from: {
+      email: string;
+      name: string;
+    };
+  };
+}
+
+export default {
+  driver: process.env.MAIL_DRIVER || 'ethereal',
+
+  defaults: {
+    from: {
+      email: 'leo.real2585@gmail.com',
+      name: 'Leandro Real',
+    },
+  },
+} as IMailConfig;
